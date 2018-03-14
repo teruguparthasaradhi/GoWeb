890ccc5b4cff49537a321e6093a9680f94c854c2
teruguparthsaradhi <terugup@gmail.com>
2018-03-14 22:36:56 +0530

    new page not found

--- pani.go
+++ pani.go
@@ -8,11 +8,18 @@ import (
 	"net/http"
 )
 func handlerFunc(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","text/html")
+if r.URL.Path=="/"{
 	fmt.Fprint(w,"<h1>welcome to my website</h1>")
-
+} else if r.URL.Path=="/contact"{
+	fmt.Fprint(w,"<h1>to get in touch please contact me at <a> href=\"mailto:terugup@gmail.com\">terugup@gmail.com</a>." )
+} else {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w,"<h1>We could not find the path</h1>")
+}
 }
 
 func main() {
-	http.HandleFunc("/home/",handlerFunc)
-	http.ListenAndServe(":3000",nil)
+	http.HandleFunc("/",handlerFunc)
+	http.ListenAndServe("localhost:3000",nil)
 }
