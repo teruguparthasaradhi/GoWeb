fbd34038c26cf54cb6f67e89ba5afc6cc3df7a7b
teruguparthsaradhi <terugup@gmail.com>
2018-03-14 21:21:12 +0530

    Added 404 page not found

--- pani.go
+++ pani.go
@@ -1,9 +1,18 @@
+/*
+Parthasaradhi terugu
+*/
 package main
 
 import (
 	"fmt"
 	"net/http"
 )
-func main() {
+func handlerFunc(w http.ResponseWriter,r *http.Request){
+	fmt.Fprint(w,"<h1>welcome to my website</h1>")
 
+}
+
+func main() {
+	http.HandleFunc("/home/",handlerFunc)
+	http.ListenAndServe(":3000",nil)
 }
